62bc298bb7279fbad6d1c367598e6cf60a4b8d22
Student8 <Student8@10.8.33.107>
2016-09-15 13:55:29 -0400

    Added more students when printing to console

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -5,6 +5,8 @@ public class ProceduralExample {
 	  String[] names = {"Jillian", "Joseph", "Jordan"};
 	  String[] years = {"Freshman", "Sophmore", "Junior"};
 	  System.out.println("Hi, my name is " + names[0] + " and I'm a " + years[0] + ".");
+	  System.out.println("Hi, my name is " + names[1] + " and I'm a " + years[1] + ".");
+	  System.out.println("Hi, my name is " + names[2] + " and I'm a " + years[2] + ".");
 	  System.out.println("Hello World!");
   }
 }
