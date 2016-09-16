b98225cff5f7eb3de82d2afbc02c9ce7d527a785
Student8 <Student8@10.8.33.107>
2016-09-16 13:21:26 -0400

    Started Work on Supporting What Year a Student is In

--- Period 8 Java/src/introduction/OOPExample.java
+++ Period 8 Java/src/introduction/OOPExample.java
@@ -3,10 +3,12 @@ package introduction;
 public class OOPExample {
 
 	public static void main(String[] args) {
-		Student jillian = new Student("Jillian");
+		Student jillian = new Senior("Jillian");
 		jillian.talk();
+		
 		Student joseph = new Student("Joseph");
 		joseph.talk();
+		
 		Student jordan = new Student("Jordan");
 		jordan.talk();
 	}
