4192a1f2f3d4eb58f259d4037056dd5fd1d06a90
Student8 <Student8@10.8.33.107>
2016-09-16 13:06:19 -0400

    Start of Object Oriented Approach

---
+++ Period 8 Java/src/introduction/OOPExample.java
@@ -0,0 +1,14 @@
+package introduction;
+
+public class OOPExample {
+
+	public static void main(String[] args) {
+		Student jillian = new Student("Jillian");
+		jillian.talk();
+		Student joseph = new Student("Joseph");
+		joseph.talk();
+		Student jordan = new Student("Jordan");
+		jordan.talk();
+	}
+
+}
---
+++ Period 8 Java/src/introduction/Student.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class Student {
+	//Fields
+	private String name;
+	
+	//Constructor (Initializes Fields)
+	public Student(String name){
+		this.name = name;
+	}
+	
+	public void talk(){
+		System.out.println("Hello, my name is " + name + ".");
+	}
+}
