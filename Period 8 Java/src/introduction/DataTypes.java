7bbf17dfad24ad9966e517bf230d938f775fe784
Student8 <Student8@10.8.33.107>
2016-09-19 13:57:35 -0400

    Lesson on Ints and Doubles

---
+++ Period 8 Java/src/introduction/DataTypes.java
@@ -0,0 +1,18 @@
+package introduction;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		System.out.println(5.0/2);
+		System.out.println((double) 5.0/2);
+		System.out.println(5/2);
+		//The third returns 2 instead of 2.5 as the first two are doubles while the third is an int which does not store decimals.
+		System.out.println(3+5.0/2+5*2);
+		System.out.println(3.0+5/2+5*2);
+		//Changing the position of the double changed the output because 5/2 is an operation using two ints and this will return an int which would be 2 instead of 2.5
+		System.out.println((int)(3.0 + 5)/(2 + 5 * 2));
+		//The first evaluates everything properly as doubles are placed in the correct places while the second one is not. The third uses a different order of operations while casted as an int which removes the decimals.
+		if(42 == 42.0) System.out.println("42 equals 42.0");
+	}
+
+}
