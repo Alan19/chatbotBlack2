ac59e87d09cb0f97104318701fac04b840d070ea
Student8 <Student8@10.8.33.106>
2016-09-29 13:29:12 -0400

    start 1.2

--- ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
+++ ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
@@ -1,5 +1,16 @@
 package groupFiles;
 
-public class SagawaGrammerBot {
+public class SagawaGrammerBot implements Chatbot {
+
+	public void talk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTriggered(String userInput) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
