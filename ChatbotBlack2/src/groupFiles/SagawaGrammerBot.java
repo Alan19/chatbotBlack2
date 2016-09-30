b53a0f2c8c3f8122630f77c2f882c97309d887d8
Student8 <Student8@10.8.33.106>
2016-09-30 13:58:18 -0400

    Grammer Method triggers

--- ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
+++ ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
@@ -10,10 +10,9 @@ public class SagawaGrammerBot implements Chatbot {
 		inGrammerLoop = true;
 		ZhenMain.println("That was a bad use of 'your'. Apologize");
 		while(inGrammerLoop){
-			ZhenMain.println("Checkmate.");
 			grammerResponse = ZhenMain.promptInput();
 			if(grammerResponse.indexOf("I'm sorry") >= 0){
-				ZhenMain.println("Apology accepted. How are you?");
+				ZhenMain.println("Apology accepted.");
 				inGrammerLoop = false;
 				ZhenMain.promptForever();
 			}
@@ -23,18 +22,15 @@ public class SagawaGrammerBot implements Chatbot {
 	//w
 	@Override
 	public boolean isTriggered(String userInput) {
-		System.out.println("check");
 		if (ZhenMain.findKeyword(userInput,  "your",  0) >= 0){
-			System.out.println("check");
 			int yourPsn = ZhenMain.findKeyword(userInput,  "your",  0);
-			String checkStringA = userInput.substring(yourPsn+4, yourPsn+7);
-			String checkStringThe = userInput.substring(yourPsn+4, yourPsn+9);
+			String checkStringA = userInput.substring(yourPsn, yourPsn+3);
+			System.out.println(checkStringA);
+			String checkStringThe = userInput.substring(yourPsn, yourPsn+5);
 			if (checkStringA.equals(" a ") || checkStringA.equals(" A ")){
-				System.out.println("check");
 				return true;
 			}
 			if (checkStringThe.toLowerCase().equals(" the ")){
-				System.out.println("check");
 				return true;
 			}
 		}
