4a8e5da63f4465ecd7d6fba1ba0f476e34db3cc9
Student 8 <akhan9054@bths.edu>
2016-09-30 13:08:48 -0400

    College Starter

--- ChatbotBlack2/src/groupFiles/AhmedCollege.java
+++ ChatbotBlack2/src/groupFiles/AhmedCollege.java
@@ -1,25 +1,25 @@
 package groupFiles;
 
-public class AhmedCollege {
+public class AhmedCollege implements Chatbot{
 	
-//	private boolean inCollegeLoop;
-//	private String collegeResponse;
+	private boolean inCollegeLoop;
+	private String collegeResponse;
 //	
-//	public void talk() {
-//		inCollegeLoop = true;
-//		while(inCollegeLoop){
-//			ZhenMain.print("(Type 'quit' to go back.)");
-//			 //static call on promptInput method from ZhenMain Class
-//			collegeResponse = ZhenMain.promptInput();
-//			if(collegeResponse.indexOf("quit") >= 0){
-//				inCollegeLoop = false;
-//				ZhenMain.promptForever();
-//			}
-//			else{
-//				ZhenMain.print("That answer was wack too.");
-//			}
-//		}
-//	}
+	public void talk() {
+		inCollegeLoop = true;
+		while(inCollegeLoop){
+			ZhenMain.println("(Type 'quit' to go back.)");
+			 //static call on promptInput method from ZhenMain Class
+			collegeResponse = ZhenMain.promptInput();
+			if(collegeResponse.indexOf("quit") >= 0){
+				inCollegeLoop = false;
+				ZhenMain.promptForever();
+			}
+			else{
+				ZhenMain.println("");
+			}
+		}
+	}
 
 	public boolean isTriggered(String userInput) {
 		//String[] triggers = {"SAT", "ACT", "college"};
