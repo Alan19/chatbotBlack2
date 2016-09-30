fc76162472100f3c5e2be3ada65b6410d22f6b54
Student8 <Student8@10.8.33.107>
2016-09-30 12:47:23 -0400

    Added trigger method

--- ChatbotBlack2/src/groupFiles/ZhenTestingBot.java
+++ ChatbotBlack2/src/groupFiles/ZhenTestingBot.java
@@ -2,6 +2,7 @@ package groupFiles;
 
 public class ZhenTestingBot implements Chatbot{
 
+	private boolean inTestingLoop;
 	@Override
 	public void talk() {
 		// TODO Auto-generated method stub
@@ -11,7 +12,12 @@ public class ZhenTestingBot implements Chatbot{
 	@Override
 	public boolean isTriggered(String userInput) {
 		String[] triggers = {"homework", "sleep", "studying", "procrastinating", "kill", "unfair", "sexist", "racist", "change classes"};
-		
+		for(int index = 0; index < triggers.length; index++){
+			if(triggers[index] == userInput){
+				inTestingLoop = true;
+				break;
+			}
+		}
 		return false;
 	}
 
