d723e511add137e39b9aa2db1db69d18970c89ae
Student8 <Student8@10.8.33.106>
2016-10-05 13:57:40 -0400

    mostly good, just adding some bells and whistles

--- ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
+++ ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
@@ -5,20 +5,35 @@ public class SagawaGrammerBot implements Chatbot {
 
 	private boolean inGrammerLoop;
 	private String grammerResponse;
+	private int grammerCount;
+	private String[] escalatingResponses = {"Say Sorry", "Look, just say you're sorry", "...is it that hard to say sorry?", "Here, let me spell it for you: S-o-r-r-y.", "I'm done talking with you. Come back when you want to apologize.", "..."};
+	public SagawaGrammerBot(){
+		grammerCount = 0;
+	}
 
 	public void talk() {
 		inGrammerLoop = true;
-		ZhenMain.println("That was a bad use of 'your'. Apologize");
+		ZhenMain.println("Oh my! Please apologize for your bad grammer - ");
+		grammerCount = 0;
 		while(inGrammerLoop){
 			grammerResponse = ZhenMain.promptInput();
-			if(grammerResponse.indexOf("I'm sorry") >= 0){
+			int lowerCase = grammerResponse.indexOf("sorry");
+			ZhenMain.println(""+lowerCase);
+			int upperCase = grammerResponse.indexOf("Sorry");
+			if((lowerCase >= 0 && noNegations(grammerResponse, lowerCase)) || (upperCase >= 0 && noNegations(grammerResponse, upperCase))){
 				ZhenMain.println("Apology accepted.");
 				inGrammerLoop = false;
 				ZhenMain.promptForever();
 			}
-			ZhenMain.println("Apologize");
+			ZhenMain.println(escalatingResponses[grammerCount]);
+			if (grammerCount < 5){
+				grammerCount++;
+			}
+			
 		}
 	}
+	
+	
 	//we
 	@Override
 	public boolean isTriggered(String userInput) {
@@ -35,6 +50,13 @@ public class SagawaGrammerBot implements Chatbot {
 			}
 		}
 		return false;
+	}
+	
+	private static boolean noNegations(String searchString, int psn) {
+		if((psn - 6 >= 0) && searchString.substring(psn-6, psn).equals("not ")){
+			return false;
+		}
+		return true;
 	}
 
 }
