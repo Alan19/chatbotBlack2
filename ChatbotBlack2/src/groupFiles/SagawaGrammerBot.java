6d3680c0204ccc40d7f3d7fd2d73d80c1fca3eb9
Student8 <Student8@10.8.33.106>
2016-10-06 13:27:13 -0400

    fragmentation is a dangerous thing

--- ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
+++ ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
@@ -13,14 +13,13 @@ public class SagawaGrammerBot implements Chatbot {
 
 	public void talk() {
 		inGrammerLoop = true;
-		ZhenMain.println("Oh my! Please apologize for your bad grammer - ");
+		ZhenMain.println("Oh my! Please apologize for your bad grammer - unless you meant to say I OWN a ");
 		grammerCount = 0;
 		while(inGrammerLoop){
 			grammerResponse = ZhenMain.promptInput();
-			int lowerCase = grammerResponse.indexOf("sorry");
+			int lowerCase = ZhenMain.findKeyword(grammerResponse,  "sorry", 0);
 			ZhenMain.println(""+lowerCase);
-			int upperCase = grammerResponse.indexOf("Sorry");
-			if((lowerCase >= 0 && noNegations(grammerResponse, lowerCase)) || (upperCase >= 0 && noNegations(grammerResponse, upperCase))){
+			if((lowerCase >= 0 && noNegations(grammerResponse, lowerCase))){
 				ZhenMain.println("Apology accepted.");
 				inGrammerLoop = false;
 				ZhenMain.promptForever();
@@ -40,20 +39,44 @@ public class SagawaGrammerBot implements Chatbot {
 		if (ZhenMain.findKeyword(userInput,  "your",  0) >= 0){
 			int yourPsn = ZhenMain.findKeyword(userInput,  "your",  0);
 			String checkStringA = userInput.substring(yourPsn, yourPsn+3);
-			System.out.println(checkStringA);
 			String checkStringThe = userInput.substring(yourPsn, yourPsn+5);
 			if (checkStringA.equals(" a ") || checkStringA.equals(" A ")){
+				//ZhenMain.println(errorFragment(userInput));
 				return true;
 			}
 			if (checkStringThe.toLowerCase().equals(" the ")){
+				//ZhenMain.println(errorFragment(userInput));
 				return true;
 			}
 		}
 		return false;
 	}
 	
+	private static String errorFragment(String searchString) {
+		int yourPsn = ZhenMain.findKeyword(searchString,  "your",  0);
+		int spaceCount = 0;
+		int trackerNumber = 1;
+		int locationOfSpace = 0;
+		int locationOfSpace2 = 0;
+		while (spaceCount < 2 && locationOfSpace < searchString.length()){
+			locationOfSpace = yourPsn + trackerNumber;
+			if (searchString.substring((yourPsn + (trackerNumber-1)), (yourPsn + trackerNumber)) == " ") {
+				spaceCount++;
+			}
+			trackerNumber++;
+		}
+		while (spaceCount < 3 && locationOfSpace < searchString.length()){
+			locationOfSpace2 = yourPsn + trackerNumber;
+			if (searchString.substring(yourPsn + (trackerNumber-1), yourPsn + trackerNumber) == " ") {
+				spaceCount++;
+			}
+			trackerNumber++;
+		}
+		return searchString.substring(locationOfSpace, (locationOfSpace2 - 1));
+	}
+	
 	private static boolean noNegations(String searchString, int psn) {
-		if((psn - 6 >= 0) && searchString.substring(psn-6, psn).equals("not ")){
+		if((psn - 6 >= 0) && searchString.substring(psn-6, psn-1).equals(" not ")){
 			return false;
 		}
 		return true;
