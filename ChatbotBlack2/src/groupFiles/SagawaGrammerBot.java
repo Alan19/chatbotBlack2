d602a1bea91f8521d8f21086ceecbada6073e425
eightbithack <shayesv@gmail.com>
2016-10-06 23:50:28 -0400

    Try now?

--- ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
+++ ChatbotBlack2/src/groupFiles/SagawaGrammerBot.java
@@ -13,13 +13,14 @@ public class SagawaGrammerBot implements Chatbot {
 
 	public void talk() {
 		inGrammerLoop = true;
+		ZhenMain.println("Oh my! Please apologize for your bad grammer");
 		grammerCount = 0;
 		while(inGrammerLoop){
 			grammerResponse = ZhenMain.promptInput();
 			checkOtherTriggers(grammerResponse);
 			int lowerCase = ZhenMain.findKeyword(grammerResponse,  "sorry", 0);
 			ZhenMain.println(escalatingResponses[grammerCount]);
-			if((lowerCase >= 0)){
+			if((lowerCase >= 0 && noNegations(grammerResponse, lowerCase))){
 				ZhenMain.println("Apology accepted.");
 				inGrammerLoop = false;
 				ZhenMain.promptForever();
@@ -40,11 +41,11 @@ public class SagawaGrammerBot implements Chatbot {
 			String checkStringA = userInput.substring(yourPsn, yourPsn+3);
 			String checkStringThe = userInput.substring(yourPsn, yourPsn+5);
 			if (checkStringA.equals(" a ") || checkStringA.equals(" A ")){
-				ZhenMain.println("Oh my! Please apologize for your bad grammer - unless you meant to say I OWN " + errorFragment(userInput));
+				//ZhenMain.println(errorFragment(userInput));
 				return true;
 			}
 			if (checkStringThe.toLowerCase().equals(" the ")){
-				ZhenMain.println("Oh my! Please apologize for your bad grammer - unless you meant to say I OWN " + errorFragment(userInput));
+				//ZhenMain.println(errorFragment(userInput));
 				return true;
 			}
 		}
@@ -53,10 +54,33 @@ public class SagawaGrammerBot implements Chatbot {
 	
 	private static String errorFragment(String searchString) {
 		int yourPsn = ZhenMain.findKeyword(searchString,  "your",  0);
-		int fuckThis = yourPsn + 5;
-		return searchString.substring(fuckThis, searchString.length());
+		int spaceCount = 0;
+		int trackerNumber = 1;
+		int locationOfSpace = 0;
+		int locationOfSpace2 = 0;
+		while (spaceCount < 2 && locationOfSpace < searchString.length()){
+			locationOfSpace = yourPsn + trackerNumber;
+			if (searchString.substring((yourPsn + (trackerNumber-1)), (yourPsn + trackerNumber)) == " ") {
+				spaceCount++;
+			}
+			trackerNumber++;
+		}
+		while (spaceCount < 3 && locationOfSpace < searchString.length()){
+			locationOfSpace2 = yourPsn + trackerNumber;
+			if (searchString.substring(yourPsn + (trackerNumber-1), yourPsn + trackerNumber) == " ") {
+				spaceCount++;
+			}
+			trackerNumber++;
+		}
+		return searchString.substring(locationOfSpace, (locationOfSpace2 - 1));
 	}
 	
+	private static boolean noNegations(String searchString, int psn) {
+		if((psn - 6 >= 0) && searchString.substring(psn-6, psn-1).equals(" not ")){
+			return false;
+		}
+		return true;
+	}
 	
 	private void checkOtherTriggers(String input){
 		if(ZhenMain.major.isTriggered(input))
